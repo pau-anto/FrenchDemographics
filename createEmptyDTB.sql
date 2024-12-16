23b59acb3cc64f0ac3a6900c3893304bd5fbb363
yaya7211 <43450496+yaya7211@users.noreply.github.com>
2024-12-16 03:49:25 +0100

    Update createEmptyDTB.sql
    Removed Annees Table

--- createEmptyDTB.sql
+++ createEmptyDTB.sql
@@ -25,23 +25,16 @@ CREATE TABLE Communes (
    CONSTRAINT fk_departement FOREIGN KEY (codeDepartement) REFERENCES Departements (codeDepartement)
 );
 
-CREATE TABLE Annees (
-   anneeSup SMALLINT UNSIGNED,
-   anneeInf SMALLINT UNSIGNED,
-   PRIMARY KEY (anneeSup, anneeInf)
-);
-
 CREATE TABLE Statistiques (
    codeCommune CHAR(5),
    anneeSup SMALLINT UNSIGNED,
    anneeInf SMALLINT UNSIGNED,
-   populationDebut BIGINT UNSIGNED,
-   populationFin BIGINT UNSIGNED,
-   naissances BIGINT UNSIGNED,
-   deces BIGINT UNSIGNED,
-   logementsDebut BIGINT UNSIGNED,
-   logementsFin BIGINT UNSIGNED,
+   populationDebut INT,
+   populationFin INT,
+   naissances INT,
+   deces INT,
+   logementsDebut INT,
+   logementsFin INT,
    PRIMARY KEY (codeCommune, anneeSup, anneeInf),
-   CONSTRAINT fk_commune FOREIGN KEY (codeCommune) REFERENCES Communes (codeCommune),
-   CONSTRAINT fk_annees FOREIGN KEY (anneeSup, anneeInf) REFERENCES Annees (anneeSup, anneeInf)
+   CONSTRAINT fk_commune FOREIGN KEY (codeCommune) REFERENCES Communes (codeCommune)
 );
